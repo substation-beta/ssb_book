b94e2a6a3335733160599b21991c278e95efa544
Youka <spanknebel.borken@t-online.de>
2019-04-06 14:40:56 +0200

    simplified book building

--- book.rs
+++ book.rs
@@ -1,12 +1,4 @@
-use std::path::PathBuf;
-use mdbook::Config;
-use mdbook::MDBook;
-
 fn main() {
-    // Output directory
-    let mut config = Config::default();
-    config.build.build_dir = PathBuf::from(concat!(env!("CARGO_MANIFEST_DIR"), "/target/book"));
-    // Compile markdown book
-    MDBook::load_with_config(&env!("CARGO_MANIFEST_DIR"), config).expect("Couldn't load book sources!")
+    mdbook::MDBook::load(&env!("CARGO_MANIFEST_DIR")).expect("Couldn't load book descriptor!")
         .build().expect("Book building failed!");
 }
