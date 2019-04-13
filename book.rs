477cbe4c3a4c32c14b64d958ced4eefc2dfd1ad9
Youka <spanknebel.borken@t-online.de>
2019-04-13 15:09:54 +0200

    replaced reference card format from svg to soffice

--- book.rs
+++ book.rs
@@ -1,16 +1,27 @@
 use mdbook::MDBook;
-use std::process::Command;
+use std::{
+    process::Command,
+    thread::sleep,
+    time::Duration
+};
 
 fn main() {
     // Compile book
     MDBook::load(&env!("CARGO_MANIFEST_DIR")).expect("Couldn't load book descriptor!")
         .build().expect("Book building failed!");
-    // Compile book SVG to PDF
-    Command::new("inkscape")
-        .args(&[
-            "--without-gui",
-            "--export-pdf=target/book/ssb_reference_card.pdf",
-            "target/book/ssb_reference_card.svg"
-        ])
-        .output().expect("Couldn't compile svg to pdf with inkscape!");
+    // Compile book FODG to PDF & SVG
+    for format in &["pdf", "svg"] {
+        Command::new("soffice")
+            .args(&[
+                "--headless",
+                "--convert-to",
+                &format!("{0}:draw_{0}_Export", format),
+                "--outdir",
+                concat!(env!("CARGO_MANIFEST_DIR"), "/target/book/"),
+                concat!(env!("CARGO_MANIFEST_DIR"), "/assets/ssb_reference_card.fodg")
+            ])
+            .output().expect(&format!("Couldn't compile fodg to {} with libreoffice!", format));
+    }
+    // Libreoffice sends commands to a remote controller (soffice.bin), so we need to wait for async processing
+    sleep(Duration::from_secs(5));
 }
