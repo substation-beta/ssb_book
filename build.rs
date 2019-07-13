2434bd8eeda0099d0ad3b51c7e6e94999f6bf644
Youka <spanknebel.borken@t-online.de>
2019-07-13 14:42:34 +0200

    improved files copy function for re-use

--- build.rs
+++ build.rs
@@ -1,5 +1,8 @@
 use mdbook::MDBook;
-use std::fs::*;
+use std::{
+    fs::{copy, create_dir_all, read_dir},
+    io::Result
+};
 
 fn main() {
     // Working directory
@@ -10,21 +13,20 @@ fn main() {
     // Copy assets
     copy(build_dir.clone() + "/assets/ssb_reference_card.pdf", build_dir.clone() + "/target/book/ssb_reference_card.pdf").expect("Couldn't copy reference card!");
     create_dir_all(build_dir.clone() + "/target/book/assets/img").expect("Couldn't create assets image directory!");
-    copy_files(build_dir.clone() + "/assets/img/", build_dir.clone() + "/target/book/assets/img/");
+    copy_files(build_dir.clone() + "/assets/img/", build_dir.clone() + "/target/book/assets/img/").expect("Couldn't copy image files to output!");
 }
 
-fn copy_files(src_dir: String, dst_dir: String) {
-    if let Ok(dir) = read_dir(src_dir) {
-        for entry in dir {
-            if let Ok(entry) = entry {
-                if let Ok(entry_type) = entry.file_type() {
-                    if entry_type.is_file() {
-                        let from = entry.path();
-                        let to = dst_dir.clone() + entry.file_name().to_str().unwrap();
-                        copy(&from, &to).expect(&format!("Couldn't copy file '{}' to '{}'!", from.display(), to));
-                    }
+fn copy_files<P: AsRef<str>>(src_dir: P, dst_dir: P) -> Result<()> {
+    for entry in read_dir(src_dir.as_ref())? {
+        if let Ok(entry) = entry {
+            if let Ok(entry_type) = entry.file_type() {
+                if entry_type.is_file() {
+                    let from = entry.path();
+                    let to = dst_dir.as_ref().to_owned() + &entry.file_name().to_string_lossy().into_owned();
+                    copy(&from, &to)?;
                 }
             }
         }
     }
+    Ok(())
 }
